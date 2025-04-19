33011eea78305c15cdebbe40e280724b3bb62185
Juan <rojuagmo1@hotmail.com>
2025-04-19 14:54:24 -0500

    primer trabajo de udemy python subido a git

--- FirstClass/ClassPython.py
+++ FirstClass/ClassPython.py
@@ -1,1 +1,9 @@
-print
+def gestionar_informacion(datos_personales):
+    nombre, edad, ciudad = datos_personales
+    nacimiento = 2025 - edad
+    datos_modificados = nombre, nacimiento, ciudad
+    return datos_personales, datos_modificados
+datos_personales = ("Ana", 30, "madrid")
+tupla_original, tupla_modificada = gestionar_informacion(datos_personales)
+print("tupla original:", tupla_original)
+print("tupla modificada:", tupla_modificada)
