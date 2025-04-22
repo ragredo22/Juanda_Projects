e4b45a2bc4302c39332498bc081bff346979c84c
ragredo22 <79950344+ragredo22@users.noreply.github.com>
2025-04-21 19:37:32 -0500

    Revert "terminacion de ejercicio practico de diccionario Udemy Python"

--- FirstClass/DiccionatiosPython.py
+++ FirstClass/DiccionatiosPython.py
@@ -3,11 +3,4 @@ def agregar_experiencia(perfil_laboral, nueva_experiencia):
     return perfil_laboral
 
 def generar_cv_reducido(perfil_laboral):
-    print(f"CV de {perfil_laboral["nombre"]}{perfil_laboral["apellido"]}")
-    print(f"Edad:{perfil_laboral["edad"]}, Ciudad:{perfil_laboral["ciudad"]}")
-    print(f"Experiencia:{perfil_laboral["experiencias"]}")
-
-perfil_laboral = {"nombre": "Ana", "apellido": "Pérez", "edad": 30, "ciudad": "Madrid", "experiencias": ["Ingenieria de software en XTZ Corp"]}
-nueva_experiencia = "Gerente de proyecto en ABC Inc"
-perfil_laboral = agregar_experiencia(perfil_laboral, nueva_experiencia)
-generar_cv_reducido(perfil_laboral)
+    print()
